a338c04e1895f0b9cb979185729b90432f5f419a
clumsy-ug <rutobe740@gmail.com>
2024-09-28 22:52:44 +0900

    スタイリング完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+
 const App = () => {
+    const elements = ['ac', "+/-", "%", "÷", 7, 8, 9, "×", 4, 5, 6, "ー", 1, 2, 3, "+", 0, ".", "⌫", "="];
+    const [sum, setSum] = useState(0); // ｢=｣を押された時にcalcDataをevalした結果
+    const [calcData, setCalcData] = useState<string[]>([]); // 計算をどんどん入れていく/｢=｣を押されたら空に戻る
+
     return (
-        <h1>Hello world</h1>
+        <div className="flex justify-center items-center min-h-screen bg-gray-200">
+            <div className="p-6 rounded-xl bg-white shadow-lg">
+                <div className="text-right text-3xl font-bold text-gray-800 mr-4">
+                    {sum}
+                    <span className="ml-1 mr-2 text-gray-800 animate-blink">|</span>
+                </div>
+
+                <div className="grid grid-cols-4 gap-1 p-6 rounded-xl">
+                    {elements.map((element, index) => (
+                        <button
+                            key={index}
+                            className={
+                                element === '='
+                                    ? "p-4 border border-slate-950 rounded-xl bg-orange-500 text-white hover:bg-orange-400"
+                                    : "p-4 border border-slate-950 rounded-xl bg-gray-700 text-white hover:bg-slate-600"
+                            }
+                        >
+                            {element}
+                        </button>
+                    ))}
+                </div>
+            </div>
+        </div>
     )   
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blink: 'blink 1.5s ease-in-out infinite'
+      },
+      keyframes: {
+        blink: {
+          '0%': { opacity: 0 },
+          '10%': { opacity: 0.2 },
+          '20%': { opacity: 0.4 },
+          '30%': { opacity: 0.6 },
+          '40%': { opacity: 1 },
+          '50%': { opacity: 1 },
+          '60%': { opacity: 1 },
+          '70%': { opacity: 0.6 },
+          '80%': { opacity: 0.3 },
+          '90%': { opacity: 0 },
+          '100%': { opacity: 0 },
+        }
+      }
+    },
   },
   plugins: [],
 }
